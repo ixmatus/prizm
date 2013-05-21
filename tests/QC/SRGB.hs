e2e19f7393b78e4301b0fa501cc312f8265bb605
Parnell Springmeyer <ixmatus@gmail.com>
2013-05-21 08:13:54 -0500

    Adding to/from hex converters and upping version

--- tests/QC/SRGB.hs
+++ tests/QC/SRGB.hs
@@ -16,11 +16,8 @@ import Data.Prizm.Types
 instance Arbitrary (RGB Integer) where
     arbitrary = liftM3 RGB (choose (0, 255)) (choose (0, 255)) (choose (0, 255))
 
-instance Arbitrary Hex where
-    arbitrary = liftM3 ((showHex (choose (0, 255))) . (showHex (choose (0, 255))) . (showHex (choose (0, 255))) "")
-
 rgb2XYZ v = C.toRGB(S.toXYZ v) == v
-rgb2HEX v = S.fromHex(S.toHex) == v
+rgb2HEX v = S.fromHex(S.toHex v) == v
 
 tests = [
     testProperty "SRGB <-> CIE XYZ" rgb2XYZ,
