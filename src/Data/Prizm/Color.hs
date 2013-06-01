d2a61fd3375b20452785f42b306f1285fea93208
Parnell Springmeyer <ixmatus@gmail.com>
2013-06-01 18:45:16 -0500

    Interpolation is now applicative

--- src/Data/Prizm/Color.hs
+++ src/Data/Prizm/Color.hs
@@ -1,7 +1,8 @@
 module Data.Prizm.Color where
 
+import Control.Applicative
+
 import Data.Prizm.Types
-import Data.Prizm.Color.CIE as C
 
 pct :: Integer -> Double
 pct = (/100) . fromIntegral . (max 0) . (min 100)
@@ -12,37 +13,31 @@ pctClamp i = max (min i 100) 0
 clamp :: Double -> Double -> Double
 clamp i clmp = max (min i clmp) 0.0
 
--- | Blend two colors using a 50% weighted average.
-blend :: (CIELCH Double, CIELCH Double) -> CIELCH Double
-blend = blendWeighted 50
+-- | Blend two colors using an interpolation value of 50%.
+(<|>) :: (CIELCH Double, CIELCH Double) -> CIELCH Double
+(<|>) = interpolate 50
 
 -- | Shade a color by blending it using a weight and the color black.
 shade :: CIELCH Double -> Percent -> CIELCH Double
-shade c w = blendWeighted (pctClamp w) (CIELCH 0.0 0.0 360.0, c)
+shade c w = interpolate (pctClamp w) (c, CIELCH 0.0 0.0 360.0)
 
 -- | Tint a color by blending it using a weight and the color white.
 tint :: CIELCH Double -> Percent -> CIELCH Double
-tint c w = blendWeighted (pctClamp w) (CIELCH 100.0 0.0 360.0, c)
+tint c w = interpolate (pctClamp w) (c, CIELCH 100.0 0.0 360.0)
 
 -- | Darken a color by converting it to CIE L*a*b* first, multiplying
 -- it by the weight, and then subtracting that value from the original
 -- L* value and converting the whole back to XYZ.
-darken :: CIEXYZ Double -> Percent -> CIEXYZ Double
-darken c w =
-    let (CIELAB l a b) = C.toLAB c
-        l' = l - (l*(pct (pctClamp w)))
-    in C.toXYZ (CIELAB l' a b)
+darken :: CIELCH Double -> Percent -> CIELCH Double
+darken (CIELCH l c h) w = (CIELCH (l - (l*(pct (pctClamp w)))) c h)
 
 -- | Lighten a color by converting it to CIE L*a*b* first, multiplying
 -- it by the weight, and then adding that value to the original L*
 -- value and converting the whole back to XYZ.
-lighten :: CIEXYZ Double -> Percent -> CIEXYZ Double
-lighten c w =
-    let (CIELAB l a b) = C.toLAB c
-        l' = l + (l*(pct (pctClamp w)))
-    in C.toXYZ (CIELAB l' a b)
+lighten :: CIELCH Double -> Percent -> CIELCH Double
+lighten (CIELCH l c h) w = (CIELCH (l + (l*(pct (pctClamp w)))) c h)
 
--- | Blend using a weighted average for two LCh colors.
+-- | Interpolate two colors
 -- 
 -- Weight is applied left to right, so if a weight of 25% is supplied,
 -- then the color on the left will be multiplied by 25% and the second
@@ -50,25 +45,14 @@ lighten c w =
 -- 
 -- CIE L*Ch is used because the interpolation between the colors is
 -- more accurate than L*ab, XYZ, and sRGB color spaces.
-blendWeighted' :: Percent -> (CIELCH Double, CIELCH Double) -> CIELCH Double
-blendWeighted' w ((CIELCH l c h),(CIELCH l' c' h')) =
-    let w'  = (pct (pctClamp w))
-        w'' = 1.0 - w'
-        nl  = clamp ((l*w') + (l'*w'')) 100
-        nc  = (c*w') + (c'*w'')
-        nh  = (h*w') + (h'*w'')
-    in CIELCH nl nc nh
+interpolate :: Percent -> (CIELCH Double, CIELCH Double) -> CIELCH Double
+interpolate w (a,b) =
+    let w' = (pct (pctClamp w))
+        (CIELCH l c h) = (-) <$> b <*> a
+        a' = (*w') <$> (CIELCH l c (shortestPath h))
+    in (+) <$> a' <*> a
 
 shortestPath :: Double -> Double
 shortestPath h | h > 180    = h - 360
                | h < (-180) = h + 360
                | otherwise  = h
-
-blendWeighted :: Percent -> (CIELCH Double, CIELCH Double) -> CIELCH Double
-blendWeighted w ((CIELCH l c h),(CIELCH l' c' h')) =
-    let w'  = (pct (pctClamp w))
-        nl  = if l > l' then l - l' else l' - l
-        nc  = c - c'
-        nh  = h - h'
-        zh  = shortestPath nh
-    in CIELCH (clamp (l + nl * w') 100) (c + nc * w') (clamp (h + zh * w') 360)
