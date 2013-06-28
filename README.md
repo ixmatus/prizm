07a069cbd97bef5acc3223e361c1d2d96b72d135
Parnell Springmeyer <ixmatus@gmail.com>
2013-06-28 09:08:47 -0500

    Upping version, fixing the API, adding new color functions, adding a visual test html page

--- README.md
+++ README.md
@@ -12,48 +12,27 @@ use in stylesheets and I wanted the same thing for use in [Clay](http://fvisser.
 There is some overlap with the Haskell Colour Module that already exists; however, this library is filling a couple of
 needs the other doesn't satisfy.
 
-## TODO
-
-This module is getting kind of messy, I want to refactor how everything is organized. I also need to fix the types.
-
 ## Supported Algorithms
 
 - sRGB <-> CIE XYZ
-- CIE XYZ <-> CIE L\*a\*b\*
-- CIE L\*\a\*b <-> CIE L\*C\*h
+- CIE XYZ <-> CIE L\*ab
+- CIE L\*ab <-> CIE L\*Ch
 
-## Roadmap
+## Supported Functions
 
-### 0.3.0.0
+- Color interpolation
+- Tinting / Darkening
+- Lightness
+- Hue
+- Chroma/Saturation
 
-I really want to clean up the API - right now stuff is just "everywhere" and the different color spaces should have
-their own modules with their own convenience conversions to/from RGB to minimize the amount of function chaining
-required.
-
-I also want to figure out a way of testing the lighten/darken, interpolation, and hue transformation functions.
-
-### 0.2.1.0
-
-Blending is now switched over to using CIE L*Ch color space for more accurate blending.
-
-### 0.2.0.0
-Functions for
-
-- color blending
-- tint / shade
-- darken / lighten
-
-### 0.1.0.4
-Conversion functions for sRGB to HEX.
-
-### 0.1.0.3
-All color types should have instances for Functor so fmap can work over their values.
-All color types should have instances for Applicative to make computations cleaner.
-
-### 0.1.0.2
-Make sure all of the conversion formulas convert losslessly at an acceptable level of precision.
+All of these functions operate on color within the CIE L\*Ch representation. The percentage values may range between
+-100 and 100.
 
 [General Color Formulas, Data, and Algorithms](http://www.brucelindbloom.com/index.html?Info.html)
+
 [CIE Conversion Mathematics](http://rip94550.wordpress.com/2011/07/04/color-cielab-and-tristimulus-xyz/)
+
 [Conversion Algorithm Sources](http://www.easyrgb.com/index.php?X=MATH&H=01)
+
 [Good list of useful color manipulation](https://github.com/mikeemoo/ColorJizz-PHP/blob/master/src/MischiefCollective/ColorJizz/ColorJizz.php)
--- src/Data/Prizm/Color.hs
+++ src/Data/Prizm/Color.hs
@@ -1,11 +1,19 @@
-module Data.Prizm.Color where
+module Data.Prizm.Color (
+  (<|>)
+, shade
+, tint
+, lightness
+, chroma
+, hue
+, interpolate
+) where
 
-import Control.Applicative
+import Control.Applicative hiding ((<|>))
 
 import Data.Prizm.Types
 
 pct :: Integer -> Double
-pct = (/100) . fromIntegral . (max 0) . (min 100)
+pct = (/100) . fromIntegral . (max (-100)) . (min 100)
 
 pctClamp :: Integer -> Integer
 pctClamp i = max (min i 100) 0
@@ -14,8 +22,8 @@ clamp :: Double -> Double -> Double
 clamp i clmp = max (min i clmp) 0.0
 
 -- | Blend two colors using an interpolation value of 50%.
-(<|>) :: (CIELCH Double, CIELCH Double) -> CIELCH Double
-(<|>) = interpolate 50
+(<|>) :: CIELCH Double -> CIELCH Double -> CIELCH Double
+(<|>) l r = interpolate 50 (l,r)
 
 -- | Shade a color by blending it using a weight and the color black.
 shade :: CIELCH Double -> Percent -> CIELCH Double
@@ -25,17 +33,19 @@ shade c w = interpolate (pctClamp w) (c, CIELCH 0.0 0.0 360.0)
 tint :: CIELCH Double -> Percent -> CIELCH Double
 tint c w = interpolate (pctClamp w) (c, CIELCH 100.0 0.0 360.0)
 
--- | Darken a color by converting it to CIE L*a*b* first, multiplying
--- it by the weight, and then subtracting that value from the original
--- L* value and converting the whole back to XYZ.
-darken :: CIELCH Double -> Percent -> CIELCH Double
-darken (CIELCH l c h) w = (CIELCH (l - (l*(pct (pctClamp w)))) c h)
+-- | Darken a color.
+lightness :: CIELCH Double -> Percent -> CIELCH Double
+lightness (CIELCH l c h) w = (CIELCH (clamp (l + (100*(pct (pctClamp w)))) 100.0) c h)
 
--- | Lighten a color by converting it to CIE L*a*b* first, multiplying
--- it by the weight, and then adding that value to the original L*
--- value and converting the whole back to XYZ.
-lighten :: CIELCH Double -> Percent -> CIELCH Double
-lighten (CIELCH l c h) w = (CIELCH (l + (l*(pct (pctClamp w)))) c h)
+-- | Change the hue of a color.
+hue :: CIELCH Double -> Percent -> CIELCH Double
+hue (CIELCH l c h) w = (CIELCH l c (clamp (h + (360*(pct (pctClamp w)))) 360.0))
+
+-- | Change the saturation/chroma of a color. A maximum chroma value
+-- of 120 is assumed here, anything more is generally considered out
+-- of gamut.
+chroma :: CIELCH Double -> Percent -> CIELCH Double
+chroma (CIELCH l c h) w = (CIELCH l (clamp (c + (120*(pct (pctClamp w)))) 120.0) h)
 
 -- | Interpolate two colors
 -- 
