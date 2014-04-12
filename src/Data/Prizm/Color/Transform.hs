2fc327edd85c035454472c20500fba1f4c468c65
Parnell Springmeyer <parnell@ixmat.us>
2014-04-11 22:01:56 -0500

    More docs and truncateN is just a synonym for roundN

--- src/Data/Prizm/Color/Transform.hs
+++ src/Data/Prizm/Color/Transform.hs
@@ -2,13 +2,15 @@
 
 module Data.Prizm.Color.Transform where
 
-import Control.Applicative
+import           Control.Applicative
 
+-- | Round a number to the Nth place.
 roundN :: Integer -> Double -> Double
 roundN n num = (fromInteger $ round $ num * (10^n)) / (10.0^^n)
 
+-- | Truncate a number to the Nth place.
 truncateN :: Integer -> Double -> Double
-truncateN n num = (fromInteger $ truncate $ num * (10^n)) / (10.0^^n)
+truncateN n num = roundN
 
 zipTransform :: ZipList Double -> [Double] -> Double
 zipTransform tv m = sum $ getZipList $ (*) <$> ZipList m <*> tv
