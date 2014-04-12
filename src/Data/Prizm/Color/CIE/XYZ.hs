54fab40e8a983b951b1898d7c08fa86de70f091e
Parnell Springmeyer <parnell@ixmat.us>
2014-04-12 11:19:33 -0500

    Cleaning up docs

--- src/Data/Prizm/Color/CIE/XYZ.hs
+++ src/Data/Prizm/Color/CIE/XYZ.hs
@@ -32,6 +32,9 @@ import {-# SOURCE #-} qualified Data.Prizm.Color.CIE.LAB      as LB
 import {-# SOURCE #-} qualified Data.Prizm.Color.CIE.LCH      as LC
 import qualified Data.Prizm.Color.SRGB         as S
 
+------------------------------------------------------------------------------
+-- Utilities
+------------------------------------------------------------------------------
 
 -- | Transform an XYZ integer to be computed against the xyzToRGB
 -- matrix.
@@ -44,6 +47,10 @@ transformLAB :: Double -> Double
 transformLAB v | v > v1    = v ** (1/3)
                | otherwise = (v2 * v) + (16 / 116)
 
+------------------------------------------------------------------------------
+-- Transform to
+------------------------------------------------------------------------------
+
 -- | Convert a CIE color to an SRGB color.
 --
 -- This function uses the default d65 illuminant matrix.
@@ -51,23 +58,18 @@ toRGB :: CIEXYZ Double -> RGB Integer
 toRGB = (toRGBMatrix d65SRGB)
 
 -- | Convert an XYZ color to an SRGB color.
+--
+-- 'XYZtoRGB' is the pre-calculated illuminant matrix, it is
+-- preferable to use 'toRG' as it uses the most "common" one.
 toRGBMatrix :: XYZtoRGB -> CIEXYZ Double -> RGB Integer
 toRGBMatrix (XYZtoRGB m) (CIEXYZ x y z) =
     let t = ZipList ((/100) <$> [x,y,z])
         [r,g,b] = (transformRGB) <$> ((zipTransform t) <$> m)
     in (S.clamp) <$> RGB r g b
 
--- | Convenience function to convert RGB to XYZ.
-fromRGB :: RGB Integer -> CIEXYZ Double
-fromRGB = S.toXYZ
-
 -- | Convenience function to convert XYZ to HEX.
 toHex :: CIEXYZ Double -> Hex
 toHex = S.toHex . toRGB
-
--- | Convenience function to convert HEX to XYZ.
-fromHex :: Hex -> CIEXYZ Double
-fromHex = S.toXYZ . S.fromHex
 
 -- | Convert an XYZ color to a LAB color.
 --
@@ -82,13 +84,25 @@ toLAB (CIEXYZ x y z) =
         b = 200 * (ty - tz)
     in CIELAB l a b
 
--- | Convenience function to convert LAB to XYZ.
-fromLAB :: CIELAB Double -> CIEXYZ Double
-fromLAB = LB.toXYZ
-
 -- | Convenience function to convert XYZ to LAB.
 toLCH :: CIEXYZ Double -> CIELCH Double
 toLCH = LB.toLCH . toLAB
+
+------------------------------------------------------------------------------
+-- Transform from
+------------------------------------------------------------------------------
+
+-- | Convenience function to convert RGB to XYZ.
+fromRGB :: RGB Integer -> CIEXYZ Double
+fromRGB = S.toXYZ
+
+-- | Convenience function to convert HEX to XYZ.
+fromHex :: Hex -> CIEXYZ Double
+fromHex = S.toXYZ . S.fromHex
+
+-- | Convenience function to convert LAB to XYZ.
+fromLAB :: CIELAB Double -> CIEXYZ Double
+fromLAB = LB.toXYZ
 
 -- | Convenience function to convert LCH to XYZ.
 fromLCH :: CIELCH Double -> CIEXYZ Double
