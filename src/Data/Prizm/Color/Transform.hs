5b2240a7846b9642eea15c23027d91da29e2588d
Parnell Springmeyer <parnell@ixmat.us>
2014-04-12 10:14:38 -0500

    Small fix!

--- src/Data/Prizm/Color/Transform.hs
+++ src/Data/Prizm/Color/Transform.hs
@@ -10,7 +10,7 @@ roundN n num = (fromInteger $ round $ num * (10^n)) / (10.0^^n)
 
 -- | Truncate a number to the Nth place.
 truncateN :: Integer -> Double -> Double
-truncateN n num = roundN
+truncateN = roundN
 
 zipTransform :: ZipList Double -> [Double] -> Double
 zipTransform tv m = sum $ getZipList $ (*) <$> ZipList m <*> tv
