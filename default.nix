44612f0a291c61550c8c3fb8fcbf1e8026f84423
Parnell Springmeyer <parnell@digitalmentat.com>
2017-03-17 09:22:11 -0500

    How could I forget the default.nix!

--- default.nix
+++ default.nix
@@ -1,19 +1,17 @@
-{ mkDerivation, base, convertible, data-default-class
-, mono-traversable, QuickCheck, stdenv, test-framework
-, test-framework-hunit, test-framework-quickcheck2, text
+{ mkDerivation, base, convertible, HUnit, mono-traversable
+, QuickCheck, stdenv, test-framework, test-framework-hunit
+, test-framework-quickcheck2, text
 }:
 mkDerivation {
   pname = "prizm";
-  version = "1.0.0";
+  version = "2.0.0";
   src = ./.;
-  libraryHaskellDepends = [
-    base convertible data-default-class mono-traversable text
-  ];
+  libraryHaskellDepends = [ base convertible mono-traversable text ];
   testHaskellDepends = [
-    base convertible mono-traversable QuickCheck test-framework
+    base convertible HUnit mono-traversable QuickCheck test-framework
     test-framework-hunit test-framework-quickcheck2
   ];
   homepage = "https://github.com/ixmatus/prizm";
-  description = "Compute with colors and differenct color spaces";
+  description = "Convert colors to different color spaces, interpolate colors, and transform colors";
   license = stdenv.lib.licenses.bsd3;
 }
